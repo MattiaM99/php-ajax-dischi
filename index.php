95e29f65aceb558d8cfc3838468d0133d2e3e39d
MattiaM99 <mattiamazzoni123@gmail.com>
2022-01-10 00:10:41 +0100

    aggiunti stili

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $songs = [
   [
     "poster" => "https://m.media-amazon.com/images/I/81Czxcf33qL._AC_SL1500_.jpg",
     "title" => "Berlin Calling",
-    "author" => ""
+    "author" => "Paul Kalkbrenner"
   ],
   [
     "poster" => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
@@ -61,6 +61,8 @@ $songs = [
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>Document</title>
 </head>
 <body>
@@ -70,14 +72,19 @@ $songs = [
 
   </div>
 
+<div class="albums container d-flex justify-content-center">
   <?php foreach($songs as $song): ?>
-  <div class="albums">
-    <img src="<?php echo $song['poster'] ?>" alt="">
-    <h5> <?php echo $song['title'] ?></h5>
-    <p><?php echo $song['author'] ?></p>
-  </div> 
-  <?php endforeach ?>
+    <div class="album">
+      <img src="<?php echo $song['poster'] ?>" alt="">
+      <h5> <?php echo $song['title'] ?></h5>
+      <p><?php echo $song['author'] ?></p>
+    </div>
+    <?php endforeach ?>
+</div> 
   <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
